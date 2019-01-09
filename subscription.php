580f5c0fef70b72b67860ba43ce5713188f6ad85
adelbentakouka <ad.bentakouka@gmail.com>
2019-01-09 14:55:53 +0100

    aaaaaaaa

---
+++ subscription.php
@@ -0,0 +1,42 @@
+
+<head>
+
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<!------ Include the above in your HEAD tag ---------->
+</head>
+
+<body id="LoginForm">
+	<div class="container">
+		<br/>
+		<h1 class="form-heading">Friendly Bets</h1>
+		<br/>
+		<div class="login-form">
+			<div class="main-div">
+				<div class="panel col-xs-">
+					<h2>Subscription</h2>
+					<p>Pleas</p>
+				</div>
+				<form id="Login">
+
+					<div class="form-group">
+
+
+						<input type="email" class="form-control" id="inputEmail" placeholder="Email Address">
+
+
+
+						<input type="password" class="form-control" id="inputPassword" placeholder="Password">
+
+					</div>
+					<div class="forgot">
+						<a href="reset.html">Forgot password?</a>
+					</div>
+
+					<button type="submit" class="btn btn-primary">Subscribed</button>
+
+				</form>
+			</div>
+		</div>
